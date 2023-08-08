11fdb326640076921304e7ad54d5930d21c1f274
JohnnyFangFangFang <johnny781222@gmail.com>
2023-08-08 18:07:23 +0800

    test Firebase hosting

---
+++ src/utils/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCKn5SlnuU392bjI1cnrF26CWS8y4JG0nw",
+  authDomain: "triviagora.firebaseapp.com",
+  projectId: "triviagora",
+  storageBucket: "triviagora.appspot.com",
+  messagingSenderId: "447610443568",
+  appId: "1:447610443568:web:e9794cc36aadbe2a1fdaf5",
+};
+
+const app = initializeApp(firebaseConfig);
