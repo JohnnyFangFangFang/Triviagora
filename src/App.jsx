189640b443c410f1a8c1a68a64afdb09bb96949d
JohnnyFangFangFang <johnny781222@gmail.com>
2023-08-15 15:31:40 +0800

    Refactor Register component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import './App.css'
+import Register from './components/Register'
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Register />
+    </>
   )
 }
 
